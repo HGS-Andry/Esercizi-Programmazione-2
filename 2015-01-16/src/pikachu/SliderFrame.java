556acb21c888818b1ac4d9cf066fb0564bd96859
HGS-Andry <HGS-Andry@users.noreply.github.com>
2015-01-16 10:24:26 +0100

    Modifica al frame e creazione di Strechableimage

--- 2015-01-16/src/pikachu/SliderFrame.java
+++ 2015-01-16/src/pikachu/SliderFrame.java
@@ -1,10 +1,16 @@
 package pikachu;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JSlider;
+
+import java.awt.Font;
 
 public class SliderFrame extends JFrame {
 
@@ -12,6 +18,29 @@ public class SliderFrame extends JFrame {
 		JPanel northPanel = new JPanel();
 		northPanel.add (new JSlider(1, 10));
 		add(northPanel, BorderLayout.NORTH);
+		
+		JPanel centerPanel=new JPanel();
+		centerPanel.setLayout(new GridLayout(2,1));
+		ButtonGroup gruppo= new ButtonGroup();
+		/*
+		JRadioButton hor=new JRadioButton("Strech Pikachu image horizontally");		
+		JRadioButton vert=new JRadioButton("Strech Pikachu image vertically");
+
+		gruppo.add(hor);
+		gruppo.add(vert);
+		
+		centerPanel.add (hor);
+		centerPanel.add (vert);
+		*/
+		
+		//addRadioButton
+		
+		add(centerPanel,BorderLayout.CENTER);
+		
+		JPanel southPanel= new JPanel();
+		southPanel.add(new StretchableImage(new ImageIcon("pika.png").getImage()));
+		
+		add(southPanel, BorderLayout.SOUTH);
 		pack();
 	}
 }
