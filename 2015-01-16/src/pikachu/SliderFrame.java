5572f884f0cd6cf963adc698a6c33296b77ea48e
HGS-Andry <HGS-Andry@users.noreply.github.com>
2015-01-16 11:11:49 +0100

    Proseguimento del progetto

--- 2015-01-16/src/pikachu/SliderFrame.java
+++ 2015-01-16/src/pikachu/SliderFrame.java
@@ -13,16 +13,25 @@ import javax.swing.JSlider;
 import java.awt.Font;
 
 public class SliderFrame extends JFrame {
-
+	
+	private final JSlider slid;
+	private StretchableImage image;
 	public SliderFrame() {
+		
 		JPanel northPanel = new JPanel();
-		northPanel.add (new JSlider(1, 10));
+		slid = new JSlider(0, 500);
+		slid.addChangeListener(event -> {
+			image.setXScaleFactor((double)slid.getValue()/100);
+			image.setYScaleFactor((double)slid.getValue()/100);
+			pack();
+		});
+		northPanel.add (slid);
 		add(northPanel, BorderLayout.NORTH);
 		
 		JPanel centerPanel=new JPanel();
 		centerPanel.setLayout(new GridLayout(2,1));
 		ButtonGroup gruppo= new ButtonGroup();
-		/*
+		
 		JRadioButton hor=new JRadioButton("Strech Pikachu image horizontally");		
 		JRadioButton vert=new JRadioButton("Strech Pikachu image vertically");
 
@@ -31,14 +40,12 @@ public class SliderFrame extends JFrame {
 		
 		centerPanel.add (hor);
 		centerPanel.add (vert);
-		*/
-		
-		//addRadioButton
 		
 		add(centerPanel,BorderLayout.CENTER);
 		
 		JPanel southPanel= new JPanel();
-		southPanel.add(new StretchableImage(new ImageIcon("pika.png").getImage()));
+		image= new StretchableImage(new ImageIcon("pika.png").getImage());
+		southPanel.add(image);
 		
 		add(southPanel, BorderLayout.SOUTH);
 		pack();
